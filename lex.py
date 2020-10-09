2e852132a739dcb021226ab3dcf697cf2569c769
Maxim <maksukh@inbox.ru>
2020-10-09 21:23:08 +0300

    Implement lexer and parser

---
+++ lex.py
@@ -0,0 +1,57 @@
+import ply.lex as lex
+
+tokens = [
+    'NUM',
+    'DELIM',
+    'BRACKET',
+    'SHTOPOR',
+    'OPERATOR',
+    'ID',
+]
+
+
+def t_NUM(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
+
+
+t_DELIM = r'\.'
+t_BRACKET = r'\(|\)'
+t_SHTOPOR = r':-'
+t_OPERATOR = r',|;'
+t_ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
+
+t_ignore = ' \t'
+
+
+def find_column(input, token):
+    line_start = input.rfind('\n', 0, token.lexpos) + 1
+    return token.lexpos - line_start + 1
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+def getLex(filename):
+    res = []
+    lexer = lex.lex()
+    with open(filename) as file:
+        content = file.readlines()
+    content = ''.join(content)
+
+    lexer.input(content)
+    file.close()
+    while True:
+        token = lexer.token()
+        if not token:
+            break
+        res.append([token.type, token.value, token.lineno, find_column(content, token)])
+    return res
---
+++ prolog_parser.py
@@ -0,0 +1,123 @@
+import sys
+from lex import *
+
+
+class Parser:
+    def __init__(self):
+        self.s = None
+        self.end = False
+        self.ignore = False
+        self.i = 0
+
+    def increment(self):
+        self.i += 1
+        if self.i >= len(self.s):
+            self.end = True
+
+    def error(self):
+        if self.ignore:
+            return False
+        if self.end:
+            raise Exception('Syntax error: unexpected end of file')
+        else:
+            line = str(self.s[self.i][2])
+            colon = str(self.s[self.i][3])
+            raise Exception('Syntax error: line ' + line + ', colon ' + colon)
+
+    def check_end(self):
+        if self.end:
+            self.ignore = False
+            self.error()
+
+    def parse_with_ignore(self, parse):
+        ignore_was = self.ignore
+        self.ignore = True
+        res = parse()
+        self.ignore = ignore_was
+        return res
+
+    def parse_list(self, parse_item, parse_sep):
+        if self.end:
+            return True
+        parse_item()
+        if not self.parse_with_ignore(parse_sep):
+            return True
+        parse_sep()
+        return self.parse_list(parse_item, parse_sep)
+
+    def parse_element(self, pos, name):
+        self.check_end()
+        if self.s[self.i][pos] != name:
+            return self.error()
+        else:
+            if not self.ignore:
+                self.increment()
+            return True
+
+    def parse_dot(self):
+        return self.parse_element(1, '.')
+
+    def parse_ID(self):
+        return self.parse_element(0, 'ID')
+
+    def parse_shtopor(self):
+        return self.parse_element(0, 'SHTOPOR')
+
+    def parse_semicolon(self):
+        return self.parse_element(1, ';')
+
+    def parse_comma(self):
+        return self.parse_element(1, ',')
+
+    def parse_opening_bracket(self):
+        return self.parse_element(1, '(')
+
+    def parse_closing_bracket(self):
+        return self.parse_element(1, ')')
+
+    def parse_brackets(self):
+        self.parse_opening_bracket()
+        self.parse_body()
+        self.parse_closing_bracket()
+
+    def parse_atom(self):
+        if self.parse_with_ignore(self.parse_opening_bracket):
+            self.parse_brackets()
+        else:
+            self.parse_ID()
+
+    def parse_M(self):
+        # M -> (ID,)*ID
+        # , = *
+        self.parse_list(self.parse_atom, self.parse_comma)
+
+    def parse_body(self):
+        # BODY -> (M;)*M
+        # ; = +
+        self.parse_list(self.parse_M, self.parse_semicolon)
+
+    def parse_R(self):
+        self.parse_ID()
+        if not self.parse_with_ignore(self.parse_dot):
+            self.parse_shtopor()
+            self.parse_body()
+        return True
+
+    def parse_S(self, s):
+        self.s = s
+        self.end = len(s) == 0
+        try:
+            while not self.end:
+                self.parse_R()
+                self.check_end()
+                self.parse_dot()
+        except Exception as e:
+            print(e)
+            return 1
+        print('No errors occurred.')
+        return 0
+
+
+S = getLex(sys.argv[1])
+parser = Parser()
+exit(parser.parse_S(S))
